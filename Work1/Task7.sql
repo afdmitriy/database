d36dda136498adcfb01ae43416ce50f174d181f7
afdmitriy <112626155+afdmitriy@users.noreply.github.com>
2022-11-09 12:30:06 +0300

    Delete Task7.sql

--- Work1/Task7.sql
+++
@@ -1,9 +0,0 @@
---7. Показать поля PersonType, NameStyle, Title, FirstName, MiddleName, LastName из таблицы Person.Person. Неизвестные значения поля Title заменить на 'Dear' (уважаемый).
---Названия полей оставить без изменений.
-SELECT PersonType
-     , NameStyle
-	 , ISNULL (Title, 'Dear') AS Title
-	 , FirstName
-	 , MiddleName
-	 , LastName
-FROM Person.Person
