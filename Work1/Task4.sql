7152f1af1f54c40d6ce9737b2bfd3941094cb8d6
afdmitriy <112626155+afdmitriy@users.noreply.github.com>
2022-11-09 12:29:51 +0300

    Delete Task4.sql

--- Work1/Task4.sql
+++
@@ -1,4 +0,0 @@
---4. Показать уникальные названия городов из таблицы Person.Address, у которых известна долгота и широта (SpatialLocation).
-SELECT DISTINCT City
-FROM Person.[Address]
-WHERE (SpatialLocation is not null);
