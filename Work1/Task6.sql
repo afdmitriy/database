5aabe2f97a3b4c0ddba950f16efcb515e8094874
afdmitriy <112626155+afdmitriy@users.noreply.github.com>
2022-11-09 12:30:01 +0300

    Delete Task6.sql

--- Work1/Task6.sql
+++
@@ -1,5 +0,0 @@
---6. Показать ID (StateProvinceID) и название (Name) регионов или штатов, а также ID страны, на территории которой они располагаются (TerritoryID) из таблицы Person.StateProvince.
---Показать только регионы из списка ('Alaska', 'Alabama', 'Colorado', 'Georgia', 'Iowa'), используя оператор IN.
-SELECT StateProvinceID, Name, TerritoryID
-FROM Person.StateProvince
-WHERE Name in ('Alaska', 'Alabama', 'Colorado', 'Georgia', 'Iowa');
