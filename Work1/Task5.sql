4be9ce2fcb1b689cda6df82b0da750ad7dd47b53
afdmitriy <112626155+afdmitriy@users.noreply.github.com>
2022-11-09 12:29:57 +0300

    Delete Task5.sql

--- Work1/Task5.sql
+++
@@ -1,4 +0,0 @@
---5. ѕоказать ID (ProductID), название (Name) и цвет (Color) товаров из таблицы Production.Product, содержащих в названии слово 'Chainring' (звездочка).
-SELECT ProductID, Name, Color
-FROM Production.Product
-WHERE Name like '%chainring%';
