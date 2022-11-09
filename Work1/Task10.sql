6f03fefb58478f1cbc22ea5b3f1a502266defd3f
afdmitriy <112626155+afdmitriy@users.noreply.github.com>
2022-11-09 12:29:36 +0300

    Delete Task10.sql

--- Work1/Task10.sql
+++
@@ -1,4 +0,0 @@
---10. ѕоказать название фирмы (Name) из таблицы Purchasing.Vendor. ≈сли фирма называетс€ 'Advanced Bicycles', заменить значение пол€ Name на NULL. Ќазвани€ полей оставить без изменений.
-SELECT NULLIF (Name, 'Advanced Bicycles') AS Name
-FROM Purchasing.Vendor
-
