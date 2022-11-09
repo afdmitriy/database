418e3210c9d5c54c6f2c31c126d087dd48f65c77
afdmitriy <112626155+afdmitriy@users.noreply.github.com>
2022-11-09 12:30:11 +0300

    Delete Task8.sql

--- Work1/Task8.sql
+++
@@ -1,7 +0,0 @@
---8. Показать поля ProductID, Name из таблицы Production.Product с применением функции COALESCE(). 
---Показать поле Measurement, так, чтобы, если значение в поле Class известно, то показать его, а иначе, показать значение в поле Color. 
---Если и в поле Color значение неизвестно, то вывести значение 'UNKNOWN'.
-SELECT ProductID, Name
-	 , COALESCE (Class, Color, 'UNKNOWN') as Measurement
-FROM Production.Product;
- 
